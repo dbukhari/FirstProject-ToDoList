db98418a4bd10c992ac003aff8a90a310bb23ba9
Duaa Bukhari <62198366+dbukhari@users.noreply.github.com>
2022-08-28 22:20:12 +0300

    Initial Commit

---
+++ FirstProject(ToDoList)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject(ToDoList)
+//
+//  Created by Duaa Bukhari on 28/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
